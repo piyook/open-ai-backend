3a6c0b29d3a8f2dc67d93f1a8369af63073c5521
piyook <piyook@piyook.com>
2023-09-29 11:37:35 +0100

    chore(): add docker env handling for API keys

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,10 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
 
+if (!process.env?.OPEN_AI_API_KEY) {
+	throw new Error('Please provide openAi API key');
+}
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (request, response) => {
 	// OR can use : res.send("Hello World!!");
-	response.json({ data: { message: 'Hello World!!' } });
+	response.json({ data: { message: `${process.env.OPEN_AI_API_KEY}` } });
 });
 
 app.listen(3000);
